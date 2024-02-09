ea251213f433e0be71aebf61faa6e82e18bbdba4
Ajay B <133206077+Ajaybalajiprasad@users.noreply.github.com>
2024-02-09 06:27:14 +0530

    Update cgpa.js

--- cgpa.js
+++ cgpa.js
@@ -3,26 +3,30 @@ let totalPoints = 0;
 
 function calculateCGPA() {
     const matCredit = 4; 
-    const phyCredit = 3;
-    const pythonCredit = 3; 
-    const chemCredit = 3;
-    const engCredit = 3;
-    const TamCredit = 1; 
+    const phyCredit = 4;
+    const codeCredit = 4; 
+    const chemCredit = 4;
+    const engCredit = 4;
+    const TamCredit = 1;
+    const aptCredit = 1;
+    const coreCredit = 4;
 
     const matGrade = parseFloat(document.getElementById('matGrade').value);
     const phyGrade = parseFloat(document.getElementById('phyGrade').value);
-    const pythonGrade = parseFloat(document.getElementById('pythonGrade').value);
+    const codeGrade = parseFloat(document.getElementById('codeGrade').value);
     const chemGrade = parseFloat(document.getElementById('chemGrade').value);
     const engGrade = parseFloat(document.getElementById('engGrade').value);
     const TamGrade = parseFloat(document.getElementById('TamGrade').value);
+    const aptGrade = parseFloat(document.getElementById('aptGrade').value);
+    const coreGrade = parseFloat(document.getElementById('coreGrade').value);
 
-    if (isNaN(matGrade) || isNaN(phyGrade) || isNaN(pythonGrade) || isNaN(chemGrade) || isNaN(engGrade) || isNaN(TamGrade) ){
-        alert('Please enter valid grades for all subjects.');
+    if (isNaN(matGrade) || isNaN(phyGrade) || isNaN(codeGrade) || isNaN(chemGrade) || isNaN(engGrade) || isNaN(TamGrade) || isNaN(aptGrade) || isNaN(codeGrade)){
+        alert('Please enter valid grades for all subjects.')
         return;
     }
 
-    totalCredits = matCredit + phyCredit + pythonCredit + chemCredit + engCredit + TamCredit;
-    totalPoints = (matCredit * matGrade) + (phyCredit * phyGrade) + (pythonCredit * pythonGrade) + (chemCredit * chemGrade) + (engCredit * engGrade) + (TamGrade * TamCredit);
+    totalCredits = matCredit + phyCredit + codeCredit + chemCredit + engCredit + TamCredit + aptCredit + coreCredit ;
+    totalPoints = (matCredit * matGrade) + (phyCredit * phyGrade) + (codeCredit * codeGrade) + (chemCredit * chemGrade) + (engCredit * engGrade) + (TamGrade * TamCredit) +(aptCredit * aptGrade) + (coreCredit * coreGrade);
 
 
     const cgpaSpan = document.getElementById('cgpa');
